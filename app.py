026ff2f9788932b403715e44ab7e8f604266e7d2
VarshaKM810 <varshakm302@gmail.com>
2026-01-29 10:56:10 +0530

    attendance

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def get_attendance_records():
     return jsonify(records)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
